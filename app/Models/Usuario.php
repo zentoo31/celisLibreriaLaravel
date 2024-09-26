08161f4df3f637eb0a6afaa270f5e94c8e9ef895
zentoo31 <zentoo31@gmail.com>
2024-09-26 00:20:00 -0500

    Fix uuid issue

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 class Usuario extends Authenticatable implements JWTSubject
 {
+    use Notifiable;
 
     protected $table = 'user'; 
 
@@ -22,6 +24,10 @@ class Usuario extends Authenticatable implements JWTSubject
         'password', 
     ];
 
+    protected $keyType = 'string'; // o 'uuid' dependiendo de la configuración
+    public $incrementing = false; // No incrementar, ya que se usa UUID
+        
+        
     use HasFactory;
 
     public function getJWTIdentifier()
